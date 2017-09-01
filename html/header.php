19131c1e081e5a1780e85cd6477426befbbac2bd
Gonzalo Llantada <gonzalo.llantada@gmail.com>
2017-09-01 20:16:24 -0300

    algun cambio hice aka

--- html/header.php
+++ html/header.php
@@ -3,18 +3,21 @@
   <head>
     <meta charset="utf-8">
     <title>Head</title>
-<link rel="stylesheet" href="css/headstyless.css">
+    <link rel="stylesheet" href="css/headstyless.css">
   </head>
   <body>
-    <div class="header">
-      <h1>SDG Premium</h1>
-<ol class="head-ol">
-  <li><a href="#">Vender</a></li>
-  <li><a href="#">Proveedores</a></li>
-  <li><a href="#">Stock</a></li>
-  <li><a href="#">Reportes</a></li>
-</ol>
+      <div class="header">
+        <h1>SDG Premium</h1>
+      <nav>
 
+        <ol class="head-ol">
+            <li><a href="#">Vender</a></li>
+            <li><a href="#">Proveedores</a></li>
+            <li><a href="#">Stock</a></li>
+            <li><a href="#">Reportes</a></li>
+        </ol>
+
+      </nav>
     </div>
 
   </body>
