bd47e1f01ebb8ed864a58bd1b8f2bcd64e52d03f
Gonzalo Llantada <gonzalo.llantada@gmail.com>
2017-08-31 22:57:38 -0300

    cambie los archivos a php

---
+++ html/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Head</title>
+
+  </head>
+  <body>
+    <div class="header">
+      <h1>SDG Premium</h1>
+<ol>
+  <li>Vender</li>
+  <li>Proveedores</li>
+  <li>Stock</li>
+  <li>Reportes</li>
+</ol>
+
+    </div>
+
+  </body>
+</html>
